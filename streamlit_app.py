bf9916090803207eb7642cacb7ba13f7d7e6690b
ShashankMehrotra <147525318+NihilistEren@users.noreply.github.com>
2025-11-14 16:40:10 +0530

    Update streamlit_app.py
    SNOWFLAKE IMPORT

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col
+from snowflake.snowpark import when_matched
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
